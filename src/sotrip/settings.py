9857b48d86b04d913f4208069b59d280f52f23ae
ym-0-bm <93938113+ym-0-bm@users.noreply.github.com>
2024-02-02 09:21:45 +0000

    mise en place environement

--- src/sotrip/settings.py
+++ src/sotrip/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,10 @@ MEDIA_URL = 'images/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
